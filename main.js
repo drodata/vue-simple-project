9105ec664125374bfacbe3d4ddbe8919f61252d9
drodata <drodata@foxmail.com>
2016-06-20 10:51:05 +0800

    set up a simple nested route example

--- main.js
+++ main.js
@@ -11,7 +11,16 @@ var Site = Vue.extend({
   template: '<p>http://drodata.com</p>'
 })
 var Blog = Vue.extend({
-  template: '<p>http://notes.drodata.com</p>'
+  template: '<div class="blog">'
+    + '<h2>Blog</h2>'
+    + '<router-view></router-view>'
+    + '</div>'
+})
+var About = Vue.extend({
+  template: '<p>This is about component in blog component</p>'
+})
+var Contact = Vue.extend({
+  template: '<p>This is contact component in blog component</p>'
 })
 
 var App = Vue.extend({})
@@ -25,7 +34,15 @@ router.map({
     component: Site
   },
   '/blog': {
-    component: Blog
+    component: Blog,
+    subRoutes: {
+      '/about': {
+        component: About
+      },
+      '/contact': {
+        component: Contact
+      }
+    }
   }
 })
 
