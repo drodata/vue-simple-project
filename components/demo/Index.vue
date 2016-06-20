44b5df0baf463597388d5342e30a96905ce3634f
drodata <drodata@foxmail.com>
2016-06-20 17:26:07 +0800

    test

---
+++ components/demo/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="demo-content">
+    <p>Current Path: <code>{{$route.path}}</code></p>
+    <p>Query: <code>{{$route.query | json}}</code></p>
+    <p>Query: <code>{{$route.flag}}</code></p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.v-link-active {
+  color: #fff;
+  background-color: #24b;
+}
+</style>
